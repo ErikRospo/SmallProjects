62255dde8a344b6427b5ebdfdb4b511d7a3558c0
Erik Rospo <rospoerik@gmail.com>
2022-06-14 14:27:36 -0700

    added some more files, for new projects

---
+++ LSBHidden/script.js
@@ -0,0 +1,113 @@
+class Steganograph {
+    constructor(canvas, image, message) {
+        /** @type {HTMLCanvasElement} */
+        this.canvas = canvas;
+        this.image = image;
+        this.message = message;
+        this.ctx = canvas.getContext('2d');
+        this.ctx.drawImage(image, 0, 0);
+        this.imageData = this.ctx.getImageData(0, 0, image.width, image.height);
+        this.data = this.imageData.data;
+        this.width = image.width;
+        this.height = image.height;
+    }
+    setbit(v, bit, value) {
+        return (v & ~(1 << bit)) | (value << bit);
+    }
+    getbit(v, bit) {
+        return (v >> bit) & 1;
+    }
+    tobinary(str) {
+        return str.split('').map(function(c) {
+            return (c.charCodeAt(0).toString(2)).padStart(8, '0');
+        }).join('');
+    }
+    frombinary(str) {
+        let r = ''
+        for (let index = 0; index < str.length; index += 8) {
+            r += String.fromCharCode(parseInt(str.substr(index, 8), 2));
+        }
+        return r;
+    }
+    encode() {
+        let message = this.message;
+        let data = this.data;
+        let binmessage = this.tobinary(message);
+        let messagebits = binmessage.split('');
+        let messagebitslength = messagebits.length;
+        for (let i = 0; i < data.length; i += 4) {
+            if (i < messagebitslength * 4) {
+                data[i] = this.setbit(data[i], 0, this.getbit(messagebits[i / 4], 0));
+            } else {
+                data[i] = this.setbit(data[i], 0, 0);
+            }
+
+        }
+        this.ctx.createImageData(this.imageData);
+        this.ctx.putImageData(this.imageData, 0, 0);
+        return this.imageData;
+    }
+    decode(imageData) {
+        // this.ctx.clearRect(0, 0, image.width, image.height);
+        this.data = this.imageData.data;
+        let c = 1
+        let recived = "";
+        for (let i = 0; i < this.data.length; i += 1) {
+            recived += this.getbit(this.data[i * 4], 0);
+            if (c >= 16) {
+                break
+            }
+            c++;
+            if (this.getbit(this.data[i * 4], 0) == 1) {
+                c = 0;
+            }
+        }
+        let recivedmessage = this.frombinary(recived);
+        return recivedmessage;
+    }
+}
+/**@type {HTMLImageElement} */
+image = new Image();
+image.src = "./work2.png";
+image.onload = function() {
+    main();
+};
+
+
+function main() {
+    canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height
+    document.body.appendChild(canvas);
+    let message;
+    /**@type {HTMLObjectElement} */
+    let messagesource = document.getElementById('message');
+    message = messagesource.data;
+
+    // message = "1.because as the sun burns heavier and heavier elements, they produce more and more energy.\n2. it would have helped, because early forms of life didn't need as much sun as their modern counterparts\n3. because it helped early thermohphiles gain and keep heat.\n3. the huronian glaciaction is accociated with a rise in oxygen.\n4. at least one.\n5. deep sea ice cores.\n6. bacteria consumed the co2, and died, turning into carbon.\n7. the land plants consume the co2 gas, thus lowering the concentration in the atmosphere. \n8. increased animals, consuming oxygen, and producing carbon dioxide, a greenhouse gas, thus increasing the tempature.\n9. because it just did. that is why. i am not going to further elaborate.\n\nEOF";
+    // console.log(message);
+    let steganograph = new Steganograph(canvas, image, message);
+    let encoded = steganograph.encode();
+    let decoded = steganograph.decode(image);
+    canvas.getContext('2d').putImageData(encoded, 0, 0);
+    let outimage = document.createElement('img');
+    outimage.width = image.width;
+    outimage.height = image.height;
+    outimage.src = canvas.toDataURL();
+    outimage.onclick = function() {
+        if (window.isSecureContext) {
+            Clipboard.copy(outimage.src);
+            alert("Image copied to clipboard");
+        } else {
+            console.log(outimage.src);
+            alert("Image output to console")
+
+        }
+    }
+    document.body.appendChild(outimage);
+    canvas.width = 0;
+    canvas.height = 0;
+    console.log(steganograph);
+    console.log(encoded);
+    console.log(decoded);
+}
